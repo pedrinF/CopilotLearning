c3edee327797b5ab499770b0491f9196e41ba58a
pedrinF <113204178+pedrinF@users.noreply.github.com>
2025-05-19 15:38:16 +0000

    Adiciona test_data.py com dados de teste para Octofit (Merington High School)

---
+++ octofit-tracker/backend/octofit_tracker/test_data.py
@@ -0,0 +1,37 @@
+# Test data for Octofit (Merington High School)
+# Based on MonaFit sample data, adapted for Octofit naming
+
+test_data = {
+    "users": [
+        {"username": "thundergod", "email": "thundergod@mhigh.edu", "password": "thundergodpassword"},
+        {"username": "metalgeek", "email": "metalgeek@mhigh.edu", "password": "metalgeekpassword"},
+        {"username": "zerocool", "email": "zerocool@mhigh.edu", "password": "zerocoolpassword"},
+        {"username": "crashoverride", "email": "crashoverride@mhigh.edu", "password": "crashoverridepassword"},
+        {"username": "sleeptoken", "email": "sleeptoken@mhigh.edu", "password": "sleeptokenpassword"},
+    ],
+    "teams": [
+        {"name": "Blue Team", "members": ["thundergod", "metalgeek", "zerocool", "crashoverride", "sleeptoken"]},
+        {"name": "Gold Team", "members": ["thundergod", "metalgeek", "zerocool", "crashoverride", "sleeptoken"]},
+    ],
+    "activities": [
+        {"user": "thundergod", "activity_type": "Cycling", "duration": "01:00:00"},
+        {"user": "metalgeek", "activity_type": "Crossfit", "duration": "02:00:00"},
+        {"user": "zerocool", "activity_type": "Running", "duration": "01:30:00"},
+        {"user": "crashoverride", "activity_type": "Strength", "duration": "00:30:00"},
+        {"user": "sleeptoken", "activity_type": "Swimming", "duration": "01:15:00"},
+    ],
+    "leaderboard": [
+        {"user": "thundergod", "score": 100},
+        {"user": "metalgeek", "score": 90},
+        {"user": "zerocool", "score": 95},
+        {"user": "crashoverride", "score": 85},
+        {"user": "sleeptoken", "score": 80},
+    ],
+    "workouts": [
+        {"name": "Cycling Training", "description": "Training for a road cycling event"},
+        {"name": "Crossfit", "description": "Training for a crossfit competition"},
+        {"name": "Running Training", "description": "Training for a marathon"},
+        {"name": "Strength Training", "description": "Training for strength"},
+        {"name": "Swimming Training", "description": "Training for a swimming competition"},
+    ]
+}
